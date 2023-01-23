ac1f4c140ef075498b514630aef0b08cf1e16fec
cutioluis <cutioluis@gmail.com>
2023-01-23 15:13:27 -0500

    add: tipos-datos.py

---
+++ src/01-tipos-de-datos.py
@@ -0,0 +1,19 @@
+#Caracteres
+"String"
+'String'
+
+
+"""Hola bienvenidos
+   Su nombre:
+   Su edad:
+   Gracias"""
+
+'''This is a text
+   with more than 2 linees:D
+'''
+#Enteros
+45
+45.3
+#Boleanos
+True
+False
---
+++ src/02.tipos-datos.py
@@ -0,0 +1,12 @@
+#Variables son un espacio de memoria de guardado.
+nombres_desarrolladores = "Alex y Luis"
+
+# Concatenacion con +
+bienvenida = "Hola " + nombres_desarrolladores + "Como están"
+
+# Concatenación con fStrings.
+bienvenida =  f"Hola {nombres_desarrolladores} Como están"
+
+# Operadores de pertenecia in y not in
+print ("estan" in bienvenida)
+print ("como" not in bienvenida)
