c3a7932b469bb97cc7cc9eb71a424a8f87d372ce
cutioluis <cutioluis@gmail.com>
2023-01-23 16:21:53 -0500

    add: Operadores Aritmeticos

---
+++ src/04_operadores_aritmeticos.py
@@ -0,0 +1,17 @@
+# Suma y resta ( + y - )
+suma = 150 + 150
+resta = 150 - 150 
+
+# Multiplicacion y division ( * y / )
+multiplicacion = 150 * 150 
+division = 150 / 150
+
+# Potenciacion ( ** )
+potenciacion = 150 ** 2 
+
+# division baja ( // )
+division_baja = 5 // 2
+
+# modulo ( % )'
+modulo = 30 % 4
+
